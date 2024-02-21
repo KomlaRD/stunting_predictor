a698aa4b917abbbe6093f331f7f8271116f700a2
komlaRD <ankueric1@gmail.com>
2024-02-21 07:04:48 +0000

    Fix: age in days and not months

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.title('Child Stunting Prediction Tool')
 age_input_method = st.radio("Select Age Input Method", ['Automatic Age Calculation', 'Manual Entry'])
 
 # Initialize an age_in_months variable
-age_in_months = None
+age = None
 
 if age_input_method == 'Automatic Age Calculation':
     # Get birth date and date of visit from user
@@ -21,28 +21,28 @@ if age_input_method == 'Automatic Age Calculation':
     date_of_assessment = st.date_input("Date of Visit", min_value=date_of_birth, value=date.today())
 
     # Function to calculate the age in months
-    def calculate_age_in_months(dob, current_date):
-        return (current_date - dob).days /30.4375
+    def calculate_age_in_days(dob, current_date):
+        return (current_date - dob).days
 
-    # Compute and show the computed age in months
-    age_in_months = calculate_age_in_months(date_of_birth, date_of_assessment)
-    st.write(f"Calculated Age in Months: {age_in_months:.2f}")
+    # Compute and show the computed age in days
+    age = calculate_age_in_days(date_of_birth, date_of_assessment)
+    st.write(f"Calculated Age in Months: {age:.2f}")
 else:
     # Allow the user to manually enter the age in months
-    age_in_months = st.number_input('Enter the Age in Months', min_value=0.0, format="%.2f", step=0.01)
+    age = st.number_input('Enter the Age in Days', min_value=0.0, format="%.2f", step=0.01)
 
 # Collect other necessary data for prediction
 weight = st.number_input('Weight in kg', step=0.01)
-height = st.number_input('Height in cm', step=0.01)
+lenhei = st.number_input('Height in cm', step=0.01)
 
 # Button for the user to click to perform the prediction
 predict_button = st.button('Predict Stunting')
 
 if predict_button:
     # Make sure the age_in_months is not None
-    if age_in_months is not None:
+    if age is not None:
         # Employ the loaded model for the stunting prediction
-        prediction = model.predict(np.array([[age_in_months, weight, height]]))
+        prediction = model.predict(np.array([[age, weight, lenhei]]))
         # Conditionally display the result of the stunting prediction
         if prediction == 1:
             st.success('The child is stunted. Refer to a nutrition professional for further assessment')
